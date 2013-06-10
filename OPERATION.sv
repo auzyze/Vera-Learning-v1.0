ee17669ebddf9ddc6e8318fd126ed9542cb311b8
Leon <auzyze@gmail.com>
2013-06-10 20:57:08 +0900

    Update OPERATION.sv

--- OPERATION.sv
+++ OPERATION.sv
@@ -3,12 +3,24 @@
 virtual class OPERATION {
 
   rand  bit[5:0]  op_len;     //duration of one operation
-  rand  bit[7:0]  payld[];    //data used to write into fifo
+        
+  rand  bit[7:0]  payld[] assoc_size op_len;    //"assoc_size"
+        
+        //note: If associtive arrays are randomized, only the elements specified in the
+        //      range declared by assoc_size are randomized
+  
+  
+  protected int min_op_dist;
+  protected int max_op_dist;
+  protected int btw_op_dist;
+  
   
   virtual task gen_payld();
 
   virtual task gen_op();
 
   virtual task prt_op();
+  
+  virtual task set_limit();
 
 }//OPERATION
